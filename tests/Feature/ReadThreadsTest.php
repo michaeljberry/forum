47f700fb05d944f183c05e1ccfcf1876a45f08d2
Michael Berry <berryintlonline@gmail.com>
2018-05-18 10:16:34 -0600

    Allow users to participate in the forum

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -28,7 +28,7 @@ class ThreadsTest extends TestCase
 
     public function test_a_user_can_read_a_single_thread()
     {
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($this->thread->title);
     }
 
@@ -40,7 +40,7 @@ class ThreadsTest extends TestCase
         $reply = factory('App\Reply')
             ->create(['thread_id' => $this->thread->id]);
 
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($reply->body);
     }
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -11,15 +11,30 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+
     public function test_a_thread_can_have_replies()
     {
-        $thread = factory('App\Thread')->create();
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     public function test_a_thread_has_a_creator()
     {
-        $thread = factory('App\Thread')->create();
-        $this->assertInstanceOf('App\User', $thread->creator);
+        $this->assertInstanceOf('App\User', $this->thread->creator);
+    }
+
+    public function test_a_thread_can_add_a_reply()
+    {
+        $this->thread->addReply([
+            'body' => 'Foobar',
+            'user_id' => 1
+        ]);
+        $this->assertCount(1, $this->thread->replies);
     }
 }
