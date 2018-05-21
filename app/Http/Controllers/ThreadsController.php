7a24855a1ec6cfae75a148e2dc909c2d1b973885
Michael Berry <berryintlonline@gmail.com>
2018-05-21 09:28:03 -0600

    Add form to create threads with tests

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -9,7 +9,7 @@ class ThreadsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index','show']);
     }
     public function index()
     {
@@ -31,5 +31,9 @@ class ThreadsController extends Controller
         ]);
 
         return redirect($thread->path());
+    }
+    public function create()
+    {
+        return view('threads.create');
     }
 }
