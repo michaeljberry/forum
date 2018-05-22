4ec466aa10642e8b5ea3ac9b8b66257324bb8fab
Michael Berry <berryintlonline@gmail.com>
2018-05-22 10:39:54 -0600

    Create validation for Threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -30,11 +30,46 @@ class CreateThreadsTest extends TestCase
     public function test_an_authenticated_user_can_create_new_forum_threads()
     {
         $this->signIn();
-        $thread = create('App\Thread');
-        $this->post('/threads', $thread->toArray());
-        $this->get($thread->path())
+
+        $thread = make('App\Thread');
+
+        $response = $this->post('/threads', $thread->toArray());
+
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
+    }
+
+    public function test_a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    public function test_a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    public function test_a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 
 }
