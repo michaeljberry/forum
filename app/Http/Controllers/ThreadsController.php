06eb76e1eabed12d4e852efc74ba43380f760e12
Michael Berry <berryintlonline@gmail.com>
2018-05-29 09:00:15 -0600

    Format Thread page; add reply pagination

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -24,7 +24,10 @@ class ThreadsController extends Controller
 
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(20)
+        ]);
     }
 
     public function store(Request $request)
