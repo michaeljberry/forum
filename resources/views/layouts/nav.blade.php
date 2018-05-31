c5021642801359739bd40e21c9ce91e3a26af5ab
Michael Berry <berryintlonline@gmail.com>
2018-05-31 09:35:47 -0600

    Restructure and refactor nav and profiles

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,71 @@
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Browse
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('threads') }}">All Threads</a>
+                        @auth
+                            <a class="dropdown-item" href="{{ route('threads') . "?by=" . auth()->user()->name }}">My Threads</a>
+                        @endauth
+                        <a href="/threads?popular=1" class="dropdown-item">Popular Threads</a>
+                    </div>
+                </li>
+                @auth
+                <li><a class="nav-link" href="{{ route('create-thread') }}">New Thread</a></li>
+                @endauth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Channels
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @foreach($channels as $channel)
+                            <a class="dropdown-item" href="{{ route('channel', ['channel' => $channel->slug]) }}">{{ $channel->name }}</a>
+                        @endforeach
+                    </div>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('profile', Auth::user()) }}">
+                                My Profile
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
     <div class="container">
-        <div class="page-header">
-            <h1>
-                {{ $profileUser->name }}
-                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-            </h1>
-        </div>
-        @foreach ($threads as $thread)
-            <div class="card">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="#">
-                                {{  $thread->creator->name }} posted:
-                            </a>
-                            {{ $thread->title }}
-                        </span>
-                        <span>
-                            {{ $thread->created_at->diffForHumans() }}
-                        </span>
+        <div class="row col-md-8 col-md-offset-2">
+            <div class="page-header">
+                <h1>
+                    {{ $profileUser->name }}
+                    <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                </h1>
+            </div>
+            @foreach ($threads as $thread)
+                <div class="card">
+                    <div class="card-header">
+                        <div class="level">
+                            <span class="flex">
+                                <a href="#">
+                                    {{  $thread->creator->name }} posted:
+                                </a>
+                                {{ $thread->title }}
+                            </span>
+                            <span>
+                                {{ $thread->created_at->diffForHumans() }}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        {{ $thread->body }}
                     </div>
                 </div>
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
+
         {{ $threads->links() }}
     </div>
 @endsection
