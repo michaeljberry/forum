8df63fbcfe0fa2c8f07037a1f38f51dd16835c87
Michael Berry <berryintlonline@gmail.com>
2018-06-05 08:35:28 -0600

    Extract Activity query with tests

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Reply;
 use App\Thread;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -40,5 +41,24 @@ class ActivityTest extends TestCase
         $reply = create(Reply::class);
 
         $this->assertEquals(Activity::count(), 2);
+    }
+
+    public function test_it_fetches_a_feed_for_any_user()
+    {
+        $this->signIn();
+        create(Thread::class, ['user_id' => auth()->id()], 2);
+
+        auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
+
+
     }
 }
