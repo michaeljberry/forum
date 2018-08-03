3b330325df61a16c18c657e79c365ef2a97b1560
Michael Berry <berryintlonline@gmail.com>
2018-08-03 10:30:28 -0600

    Extract favorites functionality to Vue component

--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -29,6 +29,21 @@ class FavoritesTest extends TestCase
         $this->assertCount(1, $reply->favorites);
     }
 
+    public function test_an_authenticated_user_can_unfavorite_a_reply()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->post('replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+
+        $this->delete('replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(0, $reply->favorites);
+    }
+
     public function test_an_authenticated_user_may_only_favorite_a_reply_once()
     {
         $this->signIn();
