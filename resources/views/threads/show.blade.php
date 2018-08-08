e239dd5a40aeb1b263b2489a091a89139da8db6c
Michael Berry <berryintlonline@gmail.com>
2018-08-08 16:36:40 -0600

    Vueify new reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -28,23 +28,15 @@
                     </div>
                 </div>
 
-                <replies :data="{{  $thread->replies }}" @removed="repliesCount--"></replies>
+                <replies
+                    :data="{{  $thread->replies }}"
+                    @added="repliesCount++"
+                    @removed="repliesCount--">
+                </replies>
 
                 <!-- {{ $replies->links() }} -->
 
-                @if (auth()->check())
-                    <form method="POST" action="{{ $thread->path() . '/replies' }} ">
-                        @csrf
-                        <div class="form-group">
-                            <textarea name="body" id="body" rows="5" class="form-control" placeholder="Have something to say?"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-default">Post</button>
-                        </div>
-                    </form>
-                @else
-                    <p class="text-center">Please <a href="{{  route('login') }}">sign in</a> to participate in this discussion.</p>
-                @endif
+
             </div>
             <div class="col-md-4">
                 <div class="card">
