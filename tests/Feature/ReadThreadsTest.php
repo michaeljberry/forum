b72ebcdf34bf5512ceac607a1f224a25e31c6e08
Michael Berry <berryintlonline@gmail.com>
2018-08-13 14:26:40 -0600

    Refactor Replies to use model events to track reply count[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[4~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -90,6 +90,18 @@ class ThreadsTest extends TestCase
         $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
     }
 
+    public function test_a_user_can_filter_threads_by_those_that_are_unanswered()
+    {
+        $thread = create(Thread::class);
+        create(Reply::class, [
+            'attributes' => [
+                'thread_id' => $thread->id
+            ]
+        ]);
+        $response = $this->getJson('threads?unanswered=1')->json();
+        $this->assertCount(1, $response);
+    }
+
     public function test_a_user_can_request_all_replies_for_a_given_thread()
     {
         $thread = create(Thread::class);
